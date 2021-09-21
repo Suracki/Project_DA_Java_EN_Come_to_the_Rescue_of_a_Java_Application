3e23b13217a9c12a828b3cb247b6a8bded1ea952
Suracki <90830022+Suracki@users.noreply.github.com>
2021-09-21 14:35:10 +0200

    Add files via upload
    Updating original code with fixes.
    
    Loads symptoms into HashMap, converts to TreeMap for sorting, and then outputs to file.

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,43 +1,76 @@
-package com.hemebiotech.analytics;
+package analytics_counter_original_fix;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
+
 	
-	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
+	public static void main(String args[]) {
+		
+		//Create HashMap to contain map of Symptom:Occurrences
+		HashMap<String, Integer> symptoms = new HashMap<String, Integer>();
+		
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
-		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
+		//Read input from file into HashMap
+		try {
+			BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
+			String line = reader.readLine();
+			
 
-			line = reader.readLine();	// get another symptom
+			while (line != null) {
+				//If symptom already exists in HashMap, increase occurrence by 1
+				if (symptoms.containsKey(line)) {
+					int value = symptoms.get(line);
+					symptoms.put(line, value + 1);
+					System.out.println("Adding duplicate");
+				}
+				//Otherwise, add symptom and set occurrence to 1
+				else {
+					symptoms.put(line, 1);
+					System.out.println("Adding new");
+				}
+				//Get next symptom from reader
+				line = reader.readLine();	
+			}		
+			reader.close();
+		}
+		catch (FileNotFoundException e) {
+			//Catch cases where symptoms file is not in the expected location
+			System.err.println("File not found: symptoms.txt");
+		}
+		catch (IOException e) {
+			//Catch any other IO errors
+			System.err.println("IO Error: " + e);
 		}
 		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
+		//Output symptom counts to file
+		
+		try {
+			FileWriter writer = new FileWriter ("result.out");
+			//Iterate through HashMap, write each symptom & occurrence number to file
+			Map<String, Integer> sorted = new TreeMap<>(symptoms);
+			
+			for (Map.Entry<String, Integer> set : sorted.entrySet()) {
+				System.out.println(set.getKey() + " = " + set.getValue());
+				writer.write(set.getKey() + " = " + set.getValue() + "\n");
+			}
+			
+			writer.close();
+			System.out.println("Total number of different symptoms: " + symptoms.size());
+		}
+		catch (IOException e) {
+			//Catch any IO errors that may occur
+			System.err.println("IO Error: " + e);
+		}
+		
+		
+		
 	}
 }
